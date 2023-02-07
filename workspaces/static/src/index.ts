217c8c3d3109ae07b6f7593b11f6b1f096ad6d42
P. Envall <petter.envall@gmail.com>
2023-02-07 12:52:41 +0100

    update dependencies

--- workspaces/static/src/index.ts
+++ workspaces/static/src/index.ts
@@ -1,12 +1,12 @@
 import App from "./App.svelte";
 const appRoot = document.querySelector(".app");
 
-let app;
+let app;  
 
 if (appRoot) {
-  app = new App({
-    target: appRoot,
-  });
+    app = new App({  
+        target: appRoot,
+    });
 }
 
 export default app;
